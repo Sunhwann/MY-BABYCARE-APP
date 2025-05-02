c3ff8a11c3c3e306486f552291603b55290524f4
Sunhwan90 <sunhwan@cuoithoi.com.vn>
2025-05-03 06:55:12 +0700

    next config js 수정

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
-};
-
-export default nextConfig;
-
-
-
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;

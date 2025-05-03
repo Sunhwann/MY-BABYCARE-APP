bb5257096d8aef2653f75af5411711b84f547070
Sunhwan90 <sunhwan@cuoithoi.com.vn>
2025-05-03 18:48:49 +0700

    next.config.ts revised

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,12 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,       // 페이지 경로 끝에 슬래시 추가
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true // 빌드 시 ESLint 오류 무시
   },
 };
 
 export default nextConfig;
-
-
-module.exports = {
-  trailingSlash: true, // 각 경로에 슬래시를 추가
-};
